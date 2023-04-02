384a946ccdfedacf19df0150ad4c07e26e818635
taikicoco <kawasakitaiki0308@gmail.com>
2023-04-03 01:57:46 +0900

    update:gp_2d

--- bandit/models/gaussian_2d.py
+++ bandit/models/gaussian_2d.py
@@ -10,8 +10,8 @@ class RBF:
     def __call__(self, x1, x2):
         if x1.shape == (0,2):
             x1 = np.array([[0,0]])
-        d = np.linalg.norm(x1-x2, axis=1, keepdims=True)
-        return self._alpha * np.exp(-d / self._beta)
+        return self._alpha * np.exp(-np.sum((x1[:, None] - x2)**2, axis=2) / self._beta)
+
 
 class GP:
     def __init__(self, mu_y, sigma, kernel):
@@ -25,8 +25,8 @@ class GP:
 
     def append(self, x, y):
         n = self._y.size
-        k0 = self._kernel(x, x)        
-        k1 = self._kernel(self._x, x)
+        k0 = self._kernel(x, x)      
+        k1 = self._kernel(x, self._x)
         k2 = self._K
         self._K = np.zeros((n + 1, n + 1))
         self._K[:n, :n] = k2
@@ -38,9 +38,42 @@ class GP:
         self._invK = np.linalg.inv(self._K + self._sigma * np.eye(n + 1))
 
     def dist(self, x):
-        k0 = (self._kernel(x, x) + self._sigma)
+        # k0 = (self._kernel(x, x) + self._sigma) 
+        k0 = np.zeros((100,)) + 0.1 
         k1 = self._kernel(x, self._x).T      
         dy = self._y - self._mu_y
         mean = self._mu_y + k1.T @ self._invK @ dy
         var = k0 - (k1 * (self._invK @ k1)).sum(axis=0)
         return st.norm(mean, np.sqrt(var))
+
+
+mu_y = 0 # 平均値
+sigma = 1  # ノイズの分散
+kernel = RBF(alpha=1, beta=1)  # カーネル関数
+model = GP(0.5, 0.1, RBF(0.3, 0.1))
+# model = GP(mu_y, sigma, kernel)  # GPモデルのインスタンスを生成
+
+y = 1
+xx = np.array([[0.2, 0.2]])
+x2 = np.array([[0.8, 0.8]])
+model.append(xx, y)
+model.append(xx, y)
+model.append(x2, 0)
+model.append(x2, 0)
+
+
+x = np.linspace(0, 1, 10, endpoint=False)
+y = np.linspace(0, 1, 10, endpoint=False)
+xx, yy = np.meshgrid(x, y)
+xdist = np.c_[xx.reshape(-1), yy.reshape(-1)] 
+d = model.dist(xdist)
+
+Z = d.mean().reshape(10,10)
+
+x = np.linspace(0, 1, 10)
+y = np.linspace(0, 1, 10)
+X, Y = np.meshgrid(x, y)
+
+plt.contourf(X, Y, Z)
+plt.colorbar()
+plt.show()
