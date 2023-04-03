ff620ece3c482f63d6f42a437f00056ebef9e1c5
taikicoco <kawasakitaiki0308@gmail.com>
2023-04-03 14:34:31 +0900

    update:gp_2d

--- bandit/models/gaussian_2d.py
+++ bandit/models/gaussian_2d.py
@@ -7,11 +7,13 @@ class RBF:
         self._alpha = alpha ** 2
         self._beta = 2 * beta ** 2
 
-    def __call__(self, x1, x2):
+    def __call__(self, x1, x2, is_k0 = False):
+        if is_k0:         
+            return self._alpha * np.exp(-np.sum((x1 - x2)**2, axis=1) / self._beta)
+
         if x1.shape == (0,2):
             x1 = np.array([[0,0]])
         return self._alpha * np.exp(-np.sum((x1[:, None] - x2)**2, axis=2) / self._beta)
-
 
 class GP:
     def __init__(self, mu_y, sigma, kernel):
@@ -38,8 +40,7 @@ class GP:
         self._invK = np.linalg.inv(self._K + self._sigma * np.eye(n + 1))
 
     def dist(self, x):
-        # k0 = (self._kernel(x, x) + self._sigma) 
-        k0 = np.zeros((100,)) + 0.1 
+        k0 = (self._kernel(x, x, is_k0=True) + self._sigma) 
         k1 = self._kernel(x, self._x).T      
         dy = self._y - self._mu_y
         mean = self._mu_y + k1.T @ self._invK @ dy
@@ -47,20 +48,16 @@ class GP:
         return st.norm(mean, np.sqrt(var))
 
 
-mu_y = 0 # 平均値
-sigma = 1  # ノイズの分散
-kernel = RBF(alpha=1, beta=1)  # カーネル関数
-model = GP(0.5, 0.1, RBF(0.3, 0.1))
-# model = GP(mu_y, sigma, kernel)  # GPモデルのインスタンスを生成
+mu_y = 0.5 # 平均値
+sigma = 0.1  # ノイズの分散
+kernel = RBF(alpha=0.3, beta=0.1)  # カーネル関数
+model = GP(mu_y, sigma, kernel)  # GPモデルのインスタンスを生成
 
 y = 1
 xx = np.array([[0.2, 0.2]])
-x2 = np.array([[0.8, 0.8]])
-model.append(xx, y)
+x2 = np.array([[0.6, 0.6]])
 model.append(xx, y)
 model.append(x2, 0)
-model.append(x2, 0)
-
 
 x = np.linspace(0, 1, 10, endpoint=False)
 y = np.linspace(0, 1, 10, endpoint=False)
@@ -69,7 +66,6 @@ xdist = np.c_[xx.reshape(-1), yy.reshape(-1)]
 d = model.dist(xdist)
 
 Z = d.mean().reshape(10,10)
-
 x = np.linspace(0, 1, 10)
 y = np.linspace(0, 1, 10)
 X, Y = np.meshgrid(x, y)
