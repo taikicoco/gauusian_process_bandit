738a0a5d783d4fa47589cfe2400952958adb44e8
taikicoco <kawasakitaiki0308@gmail.com>
2023-06-30 18:07:01 +0900

    update:gp_me in make plot img

--- work/bandit/results/make_img_gp_param.py
+++ work/bandit/results/make_img_gp_param.py
@@ -5,18 +5,18 @@ import pandas as pd
 def make_loss_contour_plot(fig, ax, df, x_col, y_col, lev, t_t_r, f_max, f_sigma):
     df_subset = df[[x_col, y_col, 'total_reward']]
     df_subset = df_subset.groupby([x_col, y_col])['total_reward'].mean().reset_index()
-    
+
     pivot_table = df_subset.pivot(index=x_col, columns=y_col, values='total_reward').fillna(0)
-    
+
     X = pivot_table.columns.values
     Y = pivot_table.index.values
     Z = t_t_r - pivot_table.values
     X, Y = np.meshgrid(X, Y)
-    
+
     c = ax.contour(X, Y, Z, colors='black', levels=lev)
-    c.clabel(fmt='%1.1f', fontsize=8) 
+    c.clabel(fmt='%1.1f', fontsize=8)
     c = ax.contourf(X, Y, Z, cmap='rainbow', levels=lev)
-    
+
     fig.colorbar(c, ax=ax, label='total_reward')
     ax.set_xlabel(y_col)
     ax.set_ylabel(x_col)
@@ -27,13 +27,15 @@ def make_parameter_analysis(df, f_max, f_sigma, algorithm):
     lev = [i for i in range(0, 190, 10)]
     n_play = 300
     t_t_r = f_max * n_play
-    
-    param_combinations = [('noise', 'alpha'), ('alpha', 'beta'), ('alpha', 'noise')]
-    
-    fig, axs = plt.subplots(1, len(param_combinations), figsize=(5*len(param_combinations), 5))
-    
-    for ax, (x_col, y_col) in zip(axs, param_combinations):
-        make_loss_contour_plot(fig, ax, df, x_col, y_col, lev, t_t_r, f_max, f_sigma)
+
+    param_combinations = [('noise', 'gp_me'), ('noise', 'alpha'), ('noise', 'beta'),
+                            ('gp_me', 'alpha'), ('gp_me', 'beta'), ('alpha', 'beta')]
+
+    fig, axs = plt.subplots(2, 3, figsize=(15, 10))  # Changed from 1 row to 2 rows and 3 columns
+
+    for i, (x_col, y_col) in enumerate(param_combinations):
+        row, col = divmod(i, 3)  # Calculate row and column for subplot
+        make_loss_contour_plot(fig, axs[row, col], df, x_col, y_col, lev, t_t_r, f_max, f_sigma)
 
     plt.tight_layout()
 
