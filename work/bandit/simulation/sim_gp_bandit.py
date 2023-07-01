4def97d51e45fdd1b5cb320b2ba54d7679afdf13
taikicoco <kawasakitaiki0308@gmail.com>
2023-07-02 02:02:51 +0900

    update:sim gp bandit

--- work/bandit/simulation/sim_gp_bandit.py
+++ work/bandit/simulation/sim_gp_bandit.py
@@ -7,16 +7,18 @@ from models.gaussian import RBF
 from models.bandit import Bandit
 from train_data.train_func import TrueFunc
 
+import google_chat_notifier
+
 # Parameters
 PARAMS = {
-    'n_play': 10,
+    'n_play': 300,
     'n_sample': 100,
     'func_sigma_list': [0.1, 0.3],
     'func_max_list': [0.2, 0.6],
-    'gp_me_list': [0, 0.5],
-    'gp_noise_list': np.round(np.geomspace(0.01, 0.5, 2), decimals=2).tolist(),
-    'rbf_alpha_list': np.round(np.geomspace(0.01, 0.5, 2), decimals=2).tolist(),
-    'rbf_beta_list': np.round(np.geomspace(0.01, 0.5, 2), decimals=2).tolist()
+    'gp_me_list': [0, 0.5, 1.0],
+    'gp_noise_list': np.round(np.geomspace(0.01, 0.5, 8), decimals=2).tolist(),
+    'rbf_alpha_list': np.round(np.geomspace(0.01, 0.5, 8), decimals=2).tolist(),
+    'rbf_beta_list': np.round(np.geomspace(0.01, 0.5, 8), decimals=2).tolist()
 }
 
 # Get the maximum seed
@@ -33,15 +35,15 @@ def generate_data(seed, algorithm):
     data2 = []
     for train_func_sigma in PARAMS['func_sigma_list']:
         for train_func_max in PARAMS['func_max_list']:
-            for gp_noise in PARAMS['gp_noise_list']:
+            for gp_me in PARAMS['gp_me_list']:
 
                 print("##############################")
                 print(f"algorithm = {algorithm}")
                 print(f"train_func_sigma = {train_func_sigma}")
                 print(f"train_func_max = {train_func_max}")
-                print(f"gp_noise = {gp_noise}")
+                print(f"gp_me = {gp_me}")
 
-                for gp_me in PARAMS['gp_me_list']:
+                for gp_noise in PARAMS['gp_noise_list']:        
                     for rbf_alpha in PARAMS['rbf_alpha_list']:
                         for rbf_beta in PARAMS['rbf_beta_list']:
                             train_func = TrueFunc(train_func_sigma, train_func_max).peak_one
@@ -80,9 +82,13 @@ if __name__ == '__main__':
     column_names1 = ['f_sigma', 'f_max', 'noise', 'gp_me', 'alpha', 'beta', 'seed', 'play_time', 'select_arm', 'reward']
     column_names2 = ['f_sigma', 'f_max', 'noise', 'gp_me', 'alpha', 'beta', 'total_reward', 'seed']
     
-    for algorithm in algorithms:
-        seed = get_max_seed(algorithm)
-        data1, data2 = generate_data(seed, algorithm)
-        save_to_csv(algorithm, data1, column_names1)
-        save_to_csv(algorithm, data2, column_names2, '_2')
-        print(f'Finished Bandit algo =  {algorithm} seed =  {seed}')
+    Traial = 1
+    for _ in range(Traial):
+        for algorithm in algorithms:
+            seed = get_max_seed(algorithm)
+            data1, data2 = generate_data(seed, algorithm)
+            save_to_csv(algorithm, data1, column_names1)
+            save_to_csv(algorithm, data2, column_names2, '_2')
+            print(f'Finished Bandit algo =  {algorithm} seed =  {seed}')
+
+    google_chat_notifier.send_message_to_google_chat("Finished Bandit simulation")
